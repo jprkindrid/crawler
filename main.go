c2ded1d2aee1a0c59720562fb10497d77d389343
jprkindrid <jpriveraa7@gmail.com>
2025-07-02 18:32:32 -0500

    basic inputs

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	println("Hello, World!")
+
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		fmt.Println("Usage: crawler <URL>")
+		os.Exit(1)
+	}
+	if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		fmt.Println("Usage: crawler <URL>")
+		os.Exit(1)
+	}
+
+	inputURL := args[0]
+	fmt.Printf("starting crawl of: %s\n", inputURL)
+
 }
